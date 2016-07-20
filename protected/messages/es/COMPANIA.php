5ac408697486f7e6f31a22c6995ebe9bcb3ab298
root <root@developer.utimpor>
2016-07-20 15:35:18 -0500

    Inicio de Repositorio

---
+++ protected/messages/es/COMPANIA.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Este Archivo contiene funciones utilizadas para Idioma Español 
+ * @author Byron Villacreses <byronvillacreses@gmail.com>
+ * @copyright Copyright &copy; Soluciones Villacreses 2014
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+return array(
+    'Company' => 'Compañía',
+    'Document' => 'Documentos',
+    'Dni' => 'Id',
+    'Ruc' => 'Ruc',
+    'Company name' => 'Razón Social',
+    'Status' => 'Estado',
+    'Document type' => 'TipoERP',
+    'Document Number' => 'NºDocERP',
+    'User' => 'Usuario',
+    'Authorization date' => 'FecAutoSRI',
+    'Authorization number SRI' => 'NºAutoSRI',
+    'User' => 'Usuario',
+    'Serving' => 'Atiende',
+    'Issuance date' => 'FecEmisión',
+    'Dni/Ruc' => 'Ced/Ruc',
+    'Company name' => 'Razón Social',
+    'Total amount' => 'ImporteTot',
+    'Send' => 'Enviado',
+    'Authorization' => 'Autorizado',
+    'Authorized' => 'Autorizado',
+    'Unauthorized' => 'No Autorizado',
+    'Returned' => 'Devuelta',
+    '-Select-' => 'Selecionar',
+    'Deny' => 'Negado',
+    'None' => 'Ninguno',
+    'All' => 'Todos',
+    'Date Start' => 'FecIni',
+    'Date End' => 'FecFin',
+    /*Botones*/
+    'Download PDF document' => 'Descargar Documento PDF',
+    'Download XML document' => 'Descargar Documento XML',
+    'Download XSD document' => 'Descargar Documento XSD',
+    'mail Send' => 'Reenviar Correo',
+    'mail Edit' => 'Editar Correo',
+    'Download' => 'Descargar',
+    /*OPCIONES BUSQUEDA*/
+    'Search by Name, Certificate' => 'Búsqueda por Nombre, Ruc',
+    'Social reason o Ruc' => 'Razón Social o Ruc',
+);
+?>
